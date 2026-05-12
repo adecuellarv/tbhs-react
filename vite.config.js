5d0687b243133334479b4a6da2c423e9021212cc
Ade Cuellar <76180286+adecuellarv@users.noreply.github.com>
2026-05-12 10:29:40 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8888/tbhs-actions/', //'https://thebesthairsalons.mx/','http://localhost:8888/tbhs-actions/'
+        target: 'http://localhost:8888/tbhs-actions/', //'https://thebesthairsalons.mx/','http://localhost:8888/tbhs-actions/' 'https://thebesthairsalon.com.mx/'
         changeOrigin: true,
         secure: false,
         rewrite: p => p.replace(/^\/api/, ''), // "/api/apis/..." -> "/apis/..."
